73b64cfcbe7fe557ee06568f6e6b229e6c248f1f
ilangi <79931569+ilangi@users.noreply.github.com>
2022-07-11 15:53:22 -0600

    Fixed naming convention

--- checkoutCreationScript.sql
+++ checkoutCreationScript.sql
@@ -1,5 +1,5 @@
 use master
---drop database itemdb
+--drop database checkoutdb
 create database checkoutdb
 GO
 
@@ -12,7 +12,7 @@ create table checkout(
 	CreditCardNumber bigint,
 	CreditCardExperationMonth int,
 	CreditCardExperationYear int,
-	CreditCardCVV int,
+	CreditCardCVV int
 );
 GO
 insert into checkout ([name],ShippingAddress,CreditCardNumber,CreditCardExperationMonth,CreditCardExperationYear,CreditCardCVV)
@@ -20,8 +20,8 @@ VALUES ('John Kaminga','500 North Street', 11122223333, 12, 2024, 888),
 ('Sammy Soo','900 South Blv', 20230034004, 3, 2023, 432),
 ('Jay Boog','2001 W 600 N', 30344445555, 1, 2026, 563)
 
---	Made items with a reference to checkout in order to store the List<Item>
-create table items
+--	Made cart with a reference to checkout in order to store the List<Item>
+create table cart
 (
 	id bigint not null IDENTITY(1,1) PRIMARY KEY,
 	checkoutID bigint not null FOREIGN KEY REFERENCES checkout(id),
@@ -31,11 +31,11 @@ create table items
 	quantity bigint
 );
 
-insert into items (title,checkoutID, details, unit_price, quantity)
+insert into cart (title,checkoutID, details, unit_price, quantity)
 	VALUES ('Wooden Chair', 1,'The best oak was used for this product', 199.99, 1),
 	('Race Chair', 1, 'The best gamers were used for this product', 599.99, 1),
 	('Office Chair', 1, 'Relax and recline as you work your deadend 9-5 job', 199.99, 1)
 
 select * from checkout
 
-select * from items
+select * from cart
