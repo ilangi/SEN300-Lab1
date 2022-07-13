93855510f620fcac619105a2a483421ea85d6c45
ilangi <79931569+ilangi@users.noreply.github.com>
2022-07-13 15:04:37 -0600

    Cors Update (Allows anywhere to connect to API)

--- Lab 1/ItemAPI/Program.cs
+++ Lab 1/ItemAPI/Program.cs
@@ -1,10 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("*")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();;
+                      });
+});
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ItemDb>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("docker_db2")));
 
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 app.MapControllers();
 
 
--- Lab 1/ItemAPI/RESTController.cs
+++ Lab 1/ItemAPI/RESTController.cs
@@ -15,7 +15,6 @@ namespace Controllers
 
 
 // GETS
-
         [HttpGet]
         public async Task<ActionResult<List<Item>>> GetAllItems()
         {
